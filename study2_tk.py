c26d603c9859c6fecc2fba1987f7cec46c910126
TXZ-PCG <ztx16@mails.tsinghua.edu.cn>
2019-01-09 15:58:59 +0800

    layout for 3, 9, 10, 15 done and verified

--- study2_tk.py
+++ study2_tk.py
@@ -9,10 +9,12 @@ import random
 
 
 class MainApplication(tk.Frame):
-    def __init__(self, parent, winsize, *args, **kwargs):
+    def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.root = parent
-        self.winsize = winsize
+        self.width = self.winfo_screenwidth()
+        self.height = self.winfo_screenheight()
+        self.winsize = (self.width, self.height)
         self.after_handles = []
         self.pats_status = []
         self.posters = []
@@ -21,12 +23,13 @@ class MainApplication(tk.Frame):
         self.posters_selected = None
         self.tkimages = []
         self.poster_size = None
+        self.poster_aratio = None
         self.pats = None
         self.pats_selected = None
         self.stop_event = threading.Event()
         self.recog = None
         self.n = 0
-        self.cases = [3, 9, 15]
+        self.cases = [3, 9, 10, 15]
         self.task_cnt = 0
         self.session_cnt = 0
         # create canvas
@@ -44,7 +47,9 @@ class MainApplication(tk.Frame):
 
     def set_winsize(self, win_size):
         self.winsize = win_size
-        self.w.configure(width=self.winsize[0], height=self.winsize[1])
+        self.width = self.winsize[0]
+        self.height = self.winsize[1]
+        self.w.configure(width=self.width, height=self.height)
         self.w.pack()
 
     def set_background(self, bg_file):
@@ -58,6 +63,8 @@ class MainApplication(tk.Frame):
         self.other_posters = self.posters[1:]
         self.target_poster = self.posters[0]
         self.poster_size = (self.target_poster.width, self.target_poster.height)
+        print(self.poster_size)
+        self.poster_aratio = float(self.target_poster.height) / float(self.target_poster.width)
 
     def set_images(self, image_seq):
         self.posters_selected = image_seq
@@ -69,9 +76,10 @@ class MainApplication(tk.Frame):
         self.n = random.sample(self.cases, 1)[0]
         self.posters_selected = random.sample(self.other_posters, self.n - 1) + [self.target_poster]
         random.shuffle(self.posters_selected)
-        self.pats_selected = self.pats[self.cases.index(self.n)]
+        for pat in self.pats:
+            if len(pat) == self.n:
+                self.pats_selected = pat
         assert len(self.posters_selected) == len(self.pats_selected)
-        # print(self.n, self.posters_selected, self.pats_selected)
 
     def id_input(self):
         pass
@@ -94,27 +102,47 @@ class MainApplication(tk.Frame):
         self.recog.set_display(self.pats_status)
 
     def display(self):
-        image_size = (int(self.poster_size[0] / 3), int(self.poster_size[1] / 3))
+        if self.n == 3:
+            self.draw(1, 3, int(self.width / 20))
+        elif self.n == 9:
+            self.draw(3, 3, int(self.height / 30))
+        elif self.n == 10:
+            self.draw(2, 5, int(self.width / 20))
+        elif self.n == 15:
+            self.draw(3, 5, int(self.height / 30))
+
+    def draw(self, n_row, n_col, padding):
+        if n_row <= 2:
+            wpadding = padding
+            lpadding = rpadding = wpadding*2
+            image_width = int((self.width - lpadding - rpadding - wpadding * (n_col - 1)) / n_col)
+            image_height = int(image_width * self.poster_aratio)
+            tpadding = bpadding = hpadding = int((self.height - n_row*image_height)/(n_row+1))
+        else:
+            hpadding = padding
+            bpadding = hpadding
+            tpadding = hpadding / 2
+            image_height = int((self.height - tpadding - bpadding - hpadding * (n_row - 1)) / n_row)
+            image_width = int(image_height / self.poster_aratio)
+            wpadding = int(image_width / n_col)
+            lpadding = rpadding = int((self.width - n_col * image_width - (n_col - 1) * wpadding) / 2)
+
         dot_size = (40, 40)
-        left_padding = 100
-        dist = 200
         for i, image in enumerate(self.posters_selected):
-            x_center = left_padding + i * dist + (i + 1) * int(image_size[0] / 2)
-            y_center = int(self.winsize[1] / 2)
-            # y_center = self.winsize[1]
-            print(x_center, y_center)
-            tkimage = ImageTk.PhotoImage(image.resize(image_size, Image.ANTIALIAS))
+            row = i % n_col
+            col = i / n_col
+            x_center = lpadding + row * (wpadding + image_width) + int(image_width / 2)
+            y_center = tpadding + col * (hpadding + image_height) + int(image_height / 2)
+            # print(col, x_center, y_center)
+            tkimage = ImageTk.PhotoImage(image.resize((image_width, image_height), Image.ANTIALIAS))
             self.tkimages.append(tkimage)
-            self.w.create_image(x_center, y_center, image=tkimage, anchor='center', tags=(str(i) + '_poster', 'poster'))
-            x_ne, y_ne = x_center + int(image_size[0] / 2), y_center - int(image_size[1] / 2)
+            self.w.create_image(x_center, y_center, image=tkimage, anchor='center',
+                                tags=(str(i) + '_poster', 'poster'))
+            x_ne, y_ne = x_center + int(image_width / 2), y_center - int(image_height / 2)
             self.w.create_rectangle(x_ne - dot_size[0], y_ne, x_ne, y_ne + dot_size[1], fill="red",
                                     tags=(str(i) + '_dot', 'dot'), outline='')
-        # print(self.w.find_withtag('poster')+self.w.find_withtag('dot'))
 
     def flash(self, item, i, idx=0):
-        # cancel the previous after function (not necessary)
-        # if self.after_handle:
-        #     self.root.after_cancel(self.after_handle)
         stipples = ['@transparent.xbm', '']
         self.w.itemconfigure(item, fill='red', stipple=stipples[idx])
         # print(self.pats_selected, i)
@@ -156,18 +184,19 @@ class MainApplication(tk.Frame):
 
 
 periods_init = [[300, 450, 650], [300, 350, 400, 500, 600, 700], [300, 350, 400, 450, 500, 600, 600, 700, 700],
+                [300, 350, 400, 450, 500, 550, 600, 650, 700, 700],
                 [300, 350, 400, 400, 450, 450, 500, 500, 600, 600, 700, 700],
                 [300, 350, 350, 400, 400, 450, 450, 500, 500, 550, 550, 600, 600, 700, 700],
                 [300, 350, 350, 400, 400, 450, 450, 500, 500, 550, 550, 600, 600, 650, 650, 650, 700, 700]]
 
 delays_init = [[0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 396, 0, 433],
-               [0, 0, 0, 200, 0, 225, 0, 320, 0, 396, 0, 433],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 467],  [0, 0, 0, 200, 0, 225, 0, 320, 0, 396, 0, 467],
                [0, 0, 175, 0, 200, 0, 225, 0, 320, 0, 362, 0, 396, 0, 433],
                [0, 0, 175, 0, 200, 0, 225, 0, 320, 0, 362, 0, 396, 0, 198, 396, 0, 433]]
 
 
 def pats_gen(periods_init, delays_init):
-    n_pats = [3, 9, 15]
+    n_pats = [3, 9, 10, 15]
     pats = [[] for _ in n_pats]
     for period, delay in zip(periods_init, delays_init):
         n = len(period)
@@ -182,9 +211,7 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.attributes("-fullscreen", False)
     # win_size = (1920, 1080)
-    win_size = (root.winfo_screenwidth(), root.winfo_screenheight())
-    print(win_size)
-    app = MainApplication(root, win_size)
+    app = MainApplication(root)
     bg_file = "./photo/bg.jpg"
     app.set_background(bg_file)
 
